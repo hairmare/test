70952115535a8baa1d826ee809bd717347bec476
Lucas Bickel <hairmare@purplehaze.ch>
2011-10-22 06:28:54 +0200

    fixed some checkstyle violations

--- src/autoload.php
+++ src/autoload.php
@@ -1,8 +1,14 @@
 <?php
 /**
  * unfinished work on autoloader
+ *
+ * @category Test
+ * @package  Test
+ * @author   Lucas Bickel <hairmare@purplehaze.ch>
+ * @license  GNU GPL
+ * @link     http://https://github.com/hairmare/test
  */
 
-ini_set('include_path'.dirname(__FILE__).'/library/',ini_get('include_path'));
+ini_set('include_path', dirname(__FILE__).'/library/', ini_get('include_path'));
 
 
