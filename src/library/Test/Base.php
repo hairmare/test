ea64d63f9e5847ed1d6be21623733b481ea5742e
Lucas Bickel <hairmare@purplehaze.ch>
2011-10-22 05:55:39 +0200

    some fun stuff for jenkins (with heaps of failures :)

---
+++ src/library/Test/Base.php
@@ -0,0 +1,9 @@
+<?php
+
+class Test_Base {
+
+    public function getName() 
+    {
+    }
+
+}
---
+++ tests/autoload.php
@@ -0,0 +1,5 @@
+<?php
+
+ini_set('include_path'.dirname(__FILE__).'/library/',ini_get('include_path'));
+
+
